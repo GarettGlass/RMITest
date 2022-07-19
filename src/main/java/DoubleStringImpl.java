1b65fe18850d02e23e422bd4d3f2f7380bd51d7e
garettglass <cheskidovalexandr@icloud.com>
2022-07-19 11:12:45 +0300

    RMI

---
+++ src/main/java/DoubleStringImpl.java
@@ -0,0 +1,7 @@
+import java.rmi.RemoteException;
+
+public class DoubleStringImpl implements DoubleString {
+    public String doubleString(String str) throws RemoteException {
+        return str + str;
+    }
+}
